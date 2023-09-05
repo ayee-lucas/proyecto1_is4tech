13d1c2d9b4bea8d8e3a5ec491a8e901537ca00ae
Alan Lopez <nouminosxz@gmail.com>
2023-09-05 12:03:48 -0600

    Added ngOnDestroy lifecycle component

--- src/app/modules/admin/components/welcome/welcome.component.ts
+++ src/app/modules/admin/components/welcome/welcome.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.scss']
 })
-export class WelcomeComponent {}
+export class WelcomeComponent implements OnDestroy {
+  ngOnDestroy() {
+    console.log({ WELCOME_ON_DESTROY: 'Welcome lifecycle ended' });
+  }
+}
