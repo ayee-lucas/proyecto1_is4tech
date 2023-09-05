be696f5898b0ba39158401ac8a38ce6da4bca847
Alan Lopez <nouminosxz@gmail.com>
2023-09-05 12:04:19 -0600

    added form component

---
+++ src/app/modules/admin/components/form/form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss']
+})
+export class FormComponent implements OnInit {
+  text = new FormControl('');
+
+  constructor() {}
+
+  ngOnInit() {}
+}
