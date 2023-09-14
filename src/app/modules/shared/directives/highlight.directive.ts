fe7709dede98e62d0ff17ccd76dbb05a15235601
Alan Lopez <nouminosxz@gmail.com>
2023-09-13 21:57:43 -0600

    Highlight improved

--- src/app/modules/shared/directives/highlight.directive.ts
+++ src/app/modules/shared/directives/highlight.directive.ts
@@ -13,6 +13,7 @@ export class HighlightDirective {
     this.element.nativeElement.style.textDecoration = 'underline';
     if (this.rgbColor) {
       this.element.nativeElement.style.textDecorationColor = `rgb(${this.rgbColor})`;
+      this.element.nativeElement.style.backgroundColor = `rgb(${this.rgbColor})`;
     }
   }
 
@@ -20,5 +21,6 @@ export class HighlightDirective {
   onOut() {
     console.log(this.rgbColor);
     this.element.nativeElement.style.textDecoration = 'unset';
+    this.element.nativeElement.style.backgroundColor = `unset`;
   }
 }
