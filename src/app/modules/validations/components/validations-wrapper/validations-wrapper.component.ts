7e7f379a8739992d41be48169561e88eeed280ec
Alan Lopez <nouminosxz@gmail.com>
2023-09-13 21:46:37 -0600

    Added validations to form group fields

--- src/app/modules/validations/components/validations-wrapper/validations-wrapper.component.ts
+++ src/app/modules/validations/components/validations-wrapper/validations-wrapper.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { NoSpecialChar } from '../../custom-validators/NoSpecialChars';
 import { NoWhiteSpace } from '../../custom-validators/NoWhiteSpace';
+import { NoLetters } from '../../custom-validators/OnlyNumbers';
 
 @Component({
   selector: 'app-validations',
@@ -11,21 +12,42 @@ import { NoWhiteSpace } from '../../custom-validators/NoWhiteSpace';
 export class ValidationsComponent {
   today = new Date();
   newClient = false;
+  showCode = false;
 
   clientForm = new FormGroup({
     name: new FormControl('', [
       Validators.required,
+      Validators.maxLength(45),
       NoSpecialChar(),
       NoWhiteSpace()
     ]),
     birth: new FormControl(new Date(), [Validators.required]),
-    nit: new FormControl('', [Validators.required]),
-    comment: new FormControl('', [Validators.required]),
-    emergencyNumber: new FormControl(''),
-    address: new FormControl('')
+    nit: new FormControl('', [
+      Validators.required,
+      NoLetters(),
+      NoSpecialChar(),
+      NoWhiteSpace()
+    ]),
+    comment: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(200),
+      NoSpecialChar(),
+      NoWhiteSpace()
+    ]),
+    emergencyNumber: new FormControl('', [
+      Validators.maxLength(8),
+      NoSpecialChar(),
+      NoWhiteSpace()
+    ]),
+    address: new FormControl('', [NoSpecialChar(), NoWhiteSpace()])
   });
 
   onSubmit() {
     console.log(this.clientForm.value);
+  }
+
+  clear() {
+    this.showCode = false;
+    this.clientForm.reset();
   }
 }
